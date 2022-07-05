34b4e4f622eac456f1c3e8e2ab42f603d3002a25
Vladimir Nortsev <vladimr.nortsev@gmail.com>
2022-07-06 00:41:31 +0400

    Finished lesson4

--- nortsev_flask_blog/__init__.py
+++ nortsev_flask_blog/__init__.py
@@ -2,22 +2,26 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+from flask_mail import Mail
 
 
 from nortsev_flask_blog.config import Config
 
+
 db = SQLAlchemy()
 login_manager = LoginManager()
 bcrypt = Bcrypt()
+mail = Mail()
 
 
-def create_app():
+def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
 
     db.init_app(app)
     login_manager.init_app(app)
     bcrypt.init_app(app)
+    mail.init_app(app)
 
     from nortsev_flask_blog.users.routes import users
     from nortsev_flask_blog.main.views import main
--- nortsev_flask_blog/config.py
+++ nortsev_flask_blog/config.py
@@ -1,3 +1,10 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SECRET_KEY = 'arh1+ysscath@70r14(-ybgz@21twkqw+yi1(idzz$wikzv#%!'
+
+    MAIL_SERVER = 'smtp.yandex.ru'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = "Lesson.geekbrains@yandex.ru"
+    MAIL_PASSWORD = "MIlana_2013"
